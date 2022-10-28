b4136cc5869e981159d2ecba8c51ebc764864492
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-10-28 22:57:40 +0200

    Added shortcut for netrw

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,4 +1,5 @@
 local map = vim.api.nvim_set_keymap
 
-map('i', 'jk'     , '<ESC>'          , {}) -- leave insert mode
-map('n', '<SPACE>', ':nohlsearch<CR>', {silent=true})
+map('i', 'jk'       , '<ESC>'              , {}) -- leave insert mode
+map('n', '<Leader>' , '<Cmd>nohlsearch<CR>', {silent=true})
+map('n', '<Leader>x', '<Cmd>Ex<CR>'        , {silent=true})
