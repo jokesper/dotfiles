292a3ef7d69126bfafb363fc846d3b2ec5adc632
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-10-28 23:31:01 +0200

    Added terminal shortcuts as comments

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,5 +1,11 @@
 local map = vim.api.nvim_set_keymap
+--local function map(modes, ...)
+--    for mode in modes:gmatch'.' do
+--        vim.api.nvim_set_keymap(mode, ...)
+--    end
+--end
 
+--map('t', '<ESC>'    , '<C-\\><C-N>'        , {}) -- leave terimal mode
 map('i', 'jk'       , '<ESC>'              , {}) -- leave insert mode
 map('n', '<Leader>n', '<Cmd>nohlsearch<CR>', {silent=true})
 map('n', '<Leader>x', '<Cmd>Ex<CR>'        , {silent=true})
