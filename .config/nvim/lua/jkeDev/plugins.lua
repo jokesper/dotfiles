9ba199225c304dc5b7361daa2b122689ed53648c
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-10-29 00:03:11 +0200

    Added bootstrapping of packer to neovim

--- .config/nvim/lua/jkeDev/plugins.lua
+++ .config/nvim/lua/jkeDev/plugins.lua
@@ -1,5 +1,19 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system{'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path}
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    else return false end
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
