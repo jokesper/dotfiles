870f049c54c57194891f6ea5b060425e1c382971
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-11-10 13:49:20 +0100

    Moved bash template inside templates/init.lua

--- .config/nvim/lua/jkeDev/templates/bash.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    vim.api.nvim_buf_set_lines(
-        0,
-        0, -1,
-        true,
-        {"#!/bin/bash",""})
-    vim.api.nvim_win_set_cursor(0, {2,0})
-end
--- .config/nvim/lua/jkeDev/templates/init.lua
+++ .config/nvim/lua/jkeDev/templates/init.lua
@@ -1,17 +1,33 @@
 local api = vim.api
 
 api.nvim_create_augroup('templates', {clear = true})
-for filetype, pattern in pairs({
-    bash = '*.sh'
+local function loader(template, ...)
+    local indent = #template:match('^%s*')
+    local cnt = {}
+    for line in (template..'\n'):format(...):gmatch('([^\n]*)\n') do
+        table.insert(cnt, line:sub(indent+1))
+    end
+    api.nvim_buf_set_lines(0, 0, -1, true, cnt)
+    api.nvim_win_set_cursor(0, {#cnt, #cnt[#cnt]})
+end
+for _,format in ipairs({
+    {
+        function(loader, _) loader([[
+            #!/bin/bash
+            ]])
+        end, '*.sh'
+    }
 }) do
+    local template = table.remove(format, 1)
+    if type(template) == 'string'
+    then template = require('jkeDev.templates.' .. template) end
+
     api.nvim_create_autocmd(
         "BufNewFile",
         {
             group = 'templates',
-            desc = string.format(
-                "A template for %s files (%s).",
-                filetype, tostring(pattern)),
-            pattern = pattern,
-            callback = require('jkeDev.templates.' .. filetype)
+            desc = "A template for files with extensions: " .. tostring(format),
+            pattern = format,
+            callback = function(event) template(loader, event) end
         })
 end
