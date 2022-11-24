10356ecec73e7c56ca3a2ccdb36fb6c6fc65b258
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-11-24 19:27:49 +0100

    Fixed menu not working on files with space

--- .config/sway/menu.sh
+++ .config/sway/menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 find -L $(sed 's/:/\/applications /g' <<< "$XDG_DATA_HOME:$XDG_DATA_DIRS:") -type f -name \*.desktop \
-    | xargs gawk -f ~/.config/sway/parse-desktop-entry.awk \
+    | xargs -d '\n' gawk -f ~/.config/sway/parse-desktop-entry.awk \
     | sort \
     | gawk -F "\t" -v menu='bemenu -ipλ' '
         {
