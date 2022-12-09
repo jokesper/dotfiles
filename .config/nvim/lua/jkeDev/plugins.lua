fc4aadacdbc202e8df069934ce1d35d91a459d7c
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-12-09 09:01:26 +0100

    Removed nvim-treesitter plugin

--- .config/nvim/lua/jkeDev/plugins.lua
+++ .config/nvim/lua/jkeDev/plugins.lua
@@ -12,7 +12,6 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
     use 'lervag/vimtex'
     use {
         'nvim-telescope/telescope.nvim',
