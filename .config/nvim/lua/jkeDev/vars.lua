e2b1896c21762661dc9f5df056baa5cd56c95588
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-12-14 17:58:13 +0100

    Changed style of netrw

--- .config/nvim/lua/jkeDev/vars.lua
+++ .config/nvim/lua/jkeDev/vars.lua
@@ -1,6 +1,8 @@
 local g   = vim.g
 local env = vim.env
 
+g.netrw_bufsettings = 'rnu nu'
+g.netrw_banner = 0
 g.vimtex_compiler_latexmk = {
     build_dir = ('%s/Documents/Output'):format(env.HOME)
 }
