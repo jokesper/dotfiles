524d196d5dec95d5612c12ea647bbb3f26f0ff33
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-12-16 17:46:58 +0100

    Added treesitter to neovim

---
+++ .config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,13 @@
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+      "bash", "awk",
+      "lua", "rust",
+      "latex",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
--- .config/nvim/lua/jkeDev/plugins.lua
+++ .config/nvim/lua/jkeDev/plugins.lua
@@ -14,6 +14,10 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
     use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    }
+    use {
         'lervag/vimtex',
         opt = true,
         event = 'BufWinEnter *.tex'
