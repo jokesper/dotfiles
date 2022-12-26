380e5bf00a78aa21e54f48b9845c78484ee866e9
jkeDev <52346246+jkeDev@users.noreply.github.com>
2022-12-26 23:58:37 +0100

    Removed unnecessary options from neovim config

--- .config/nvim/lua/jkeDev/init.lua
+++ .config/nvim/lua/jkeDev/init.lua
@@ -1,5 +1,4 @@
-vim.g.     mapleader = ' '
-vim.g.maplocalleader = ','
+vim.g.mapleader = ' '
 
 require 'jkeDev.libext'
 require 'jkeDev.vars'
--- .config/nvim/lua/jkeDev/opts.lua
+++ .config/nvim/lua/jkeDev/opts.lua
@@ -5,57 +5,41 @@ opt.title          = true
 opt.titlestring    = "%<%F - nvim"
 opt.relativenumber = true
 opt.number         = true
-opt.whichwrap      = "lh<>"
 
 -- FIXME:
 -- 1. Scrolloff faction is hard coded
--- 2. O(n) instead of O(1) for n = number of windows when opening a new window
--- 3. For NeoVim < 0.8 nvim_create_autocmd is not supported
+-- 2. For NeoVim < 0.8 nvim_create_autocmd is not supported
+api.nvim_create_augroup('options', {clear = true})
 api.nvim_create_autocmd({"VimEnter","VimResume","VimResized","WinNew","WinClosed"}, {
+	group = 'options',
     desc = "Update scrolloff value",
     callback = function()
-        for _,id in pairs(vim.api.nvim_list_wins()) do
-            vim.wo[id].scrolloff =
-                math.ceil(api.nvim_win_get_height(id) / 10)
-            vim.wo[id].sidescrolloff =
+		for _,id in pairs(api.nvim_list_wins()) do
+			local wo = vim.wo[id]
+			wo.scrolloff, wo.sidescrolloff =
+				math.ceil(api.nvim_win_get_height(id) / 10),
                 math.ceil(api.nvim_win_get_width (id) / 10)
         end
-    end})
--- Possible fix for the 2.:
--- api.nvim_create_autocmd({"WinNew", "WinScrolled"}, {
---    desc = "Update scrolloff value"
---    callback = function()
---    end})
+	end,
+})
 
-opt.encoding       = 'utf8'
-opt.fileencoding   = 'utf8'
 opt.linebreak      = true
 
-opt.syntax         = "ON"
 opt.termguicolors  = true
-opt.spelllang      = "en"
+
+opt.list = true
+opt.listchars = 'tab:| '
 
 opt.cursorline     = true
 opt.cursorlineopt  = "number"
 
-opt.foldmethod     = "indent"
-opt.foldenable     = false
-
 opt.ignorecase     = true
 opt.smartcase      = true
---opt.incsearch      = true
---opt.hlsearch       = true
 
-opt.expandtab      = true
 opt.shiftwidth     = 4
-opt.shiftround     = true
---opt.smarttab       = true
 opt.softtabstop    = 4
 opt.tabstop        = 4
---opt.autoindent     = true
 opt.smartindent    = true
 
 opt.splitright     = true
 opt.splitbelow     = true
-
---opt.lazyredraw = true
