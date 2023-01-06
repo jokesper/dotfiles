d7b32e7c0a9910901c84ddeb14e7a29eea2c9ecf
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-01-07 00:29:57 +0100

    Automatically entering terminal mode and closing terminal window after exit

--- .config/nvim/lua/jkeDev/autocmds.lua
+++ .config/nvim/lua/jkeDev/autocmds.lua
@@ -1,9 +1,22 @@
 local api = vim.api
-local augroup, autocmd = api.nvim_create_augroup, api.nvim_create_autocmd
+local augroup = api.nvim_create_augroup
+local autocmd = api.nvim_create_autocmd
 
 augroup('custom', {clear = true})
 autocmd('TermOpen', {
     group = 'custom',
     desc = "Automatically enter Terminal-mode when opening a terminal.",
     command = 'startinsert'
+})
+autocmd('TermClose', {
+	group = 'custom',
+	pattern = 'term://*:*{bash}',
+	desc = 'Automatically close terminal window when exiting shells.',
+	command = 'quit!',
+})
+autocmd('WinEnter', {
+	group = 'custom',
+	pattern = 'term://*',
+	desc = 'Automatically enter Terminal-mode when entering a terminal window.',
+	command = 'startinsert',
 })
