e0d1a05db24dbe25fbc52691ac54c5c90f2fd289
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-01-08 01:05:50 +0100

    Fixed netrw having problems with modified buffers

--- .config/nvim/lua/jkeDev/vars.lua
+++ .config/nvim/lua/jkeDev/vars.lua
@@ -1,7 +1,9 @@
 local g   = vim.g
 local env = vim.env
 
-g.netrw_bufsettings = 'rnu nu'
+-- Some settings should always be there for netrw to function properly
+-- see `:echo g:netrw_bufsettings` when in `nvim --clean`
+g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro rnu'
 g.netrw_banner = 0
 g.vimtex_compiler_latexmk = {
     build_dir = ('%s/Documents/Output'):format(env.HOME)
