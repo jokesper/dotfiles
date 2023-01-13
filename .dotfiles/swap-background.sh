3e49d8543155a4537cf7ab0e2e41cfb174e25be0
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-01-13 22:45:06 +0100

    Now changing background based on current network

---
+++ .dotfiles/swap-background.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+fallback='#0F0F0F'
+if [ -z $1 ]; then
+	WPA_ID=$(wpa_cli status | awk -F= '/^id=/ {print $2; exit}')
+	wpa_cli -Ba "$0"
+fi
+case ${2:-$([[ -n $WPA_ID ]] && echo "CONNECTED")} in
+	CONNECTED)
+		ssid=$(wpa_cli get_network "$WPA_ID" ssid \
+			| tail -n 1 \
+			| jq --raw-output .)
+		swaymsg "output * background ~/Desktop/background-$ssid fit $fallback"
+		;;
+	*)
+		swaymsg "output * background $fallback solid_color"
+		;;
+esac
