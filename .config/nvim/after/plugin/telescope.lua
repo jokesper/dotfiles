6f64294c03f88678f687e696a94c3ec24c7f3f1d
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-01-17 21:32:55 +0100

    Added live grep to nvim

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,10 @@
 local telescope = require 'telescope.builtin'
 local map = vim.keymap.set
 
-map('n', '<Leader>f'    , telescope.find_files, {})
-map('n', '<Leader><Tab>', telescope. git_files, {})
+require 'telescope'.setup {
+	defaults = {file_ignore_patterns = {'Output/'}},
+}
+
+map('n', '<Leader>f', telescope.find_files)
+map('n', '<Leader><Tab>', telescope.git_files)
+map('n', '<Leader>s', telescope.live_grep)
