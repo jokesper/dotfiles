c9617f3360bb1493ceb163fd41b30854772279b4
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-02-09 10:52:33 +0100

    Enabled color in default update script

--- .dotfiles/defaults/update.sh
+++ .dotfiles/defaults/update.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-echo "==> $USER"
+lv1="\e[32m==>\e[0m"
+lv2="\e[34m  ->\e[0m"
+printf "$lv1 $USER\n"
 
-echo "  -> Synchronizing music playlists"
+printf "$lv2 Synchronizing music playlists\n"
 ~/.dotfiles/music-sync.sh
 
-echo "  -> Updating neovim plugins"
+printf "$lv2 Updating neovim plugins\n"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c PackerSync
