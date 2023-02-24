8e5b6c462c0a3faf63b2e62870e6b651ce608315
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-02-24 14:51:50 +0100

    Added default doas.conf

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -3,4 +3,5 @@
 cd "${0%/*}/install/"
 install -Dm644 ./user-updates.hook -t /etc/pacman.d/hooks/
 install -Dm755 ./user-updates.sh -t /opt/dotfiles/
+install -Dm644 ./doas.conf -t /etc/doas.conf
 runuser -u "${DOAS_USER:-$SUDO_USER}" ./../setup-user.sh
