c0075e5e1e1fa4c60d6ecefa4faf5e22b1e13cb7
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-03-21 12:17:02 +0100

    Changed default timeout on failed login attempts

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -5,6 +5,7 @@ cd "$path/install/"
 install -Dm644 ./user-updates.hook -t /etc/pacman.d/hooks/
 install -Dm755 ./user-updates.sh -t /opt/dotfiles/
 install -Dm644 ./doas.conf -t /etc/
+install -Dm644 ./faillock.conf -t /etc/security/
 
 user=${DOAS_USER:-$SUDO_USER}
 if [ -z $user ]; then
