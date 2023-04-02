e7ec4e06d9e39ea9fd88da6f1ef45b18526a4c27
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-02 18:12:07 +0200

    Fixed man not working in normal terminal emulators

--- .dotfiles/man.sh
+++ .dotfiles/man.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n $NVIM ] && [[ "$*" =~ ^[-_.[:alnum:][:blank:]]+$ ]]; then
+if [[ -n $NVIM ]] && [[ "$*" =~ ^[-_.[:alnum:][:blank:]]+$ ]]; then
 	nvim --server "$NVIM" --remote-send "<Cmd>tab Man $*<CR>"
 else
 	man "$@"
