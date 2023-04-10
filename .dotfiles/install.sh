116c7e2d1ef5e622b8602cc5232cdf96aece7191
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-10 21:24:50 +0200

    Swapped  Ctrl, Caps and Esc in sway

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -7,4 +7,5 @@ install -Dm755 ./user-updates.sh -t /opt/dotfiles/
 install -Dm644 ./doas.conf -t /etc/
 install -Dm644 ./faillock.conf -t /etc/security/
 install -Dm644 ./personal.map -t /usr/local/share/kbd/keymaps/
+install -Dm644 ./personal-xkb -T /usr/share/X11/xkb/symbols/personal
 install -Dm644 ./vconsole.conf -t /etc/
