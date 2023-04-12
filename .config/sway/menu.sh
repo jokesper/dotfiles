8a1aba5d8583e8f0b0afe670f4a327f4d671334c
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-12 14:26:06 +0200

    Made sway config use XDG_CONFIG_HOME for other files

--- .config/sway/menu.sh
+++ .config/sway/menu.sh
@@ -3,7 +3,7 @@ find \
 	-L $(sed 's/:/\/applications /g' <(printf "%q" \
 		"${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}:")) \
 	-type f -name \*.desktop \
-	| xargs -d '\n' gawk -f ~/.config/sway/parse-desktop-entry.awk \
+	| xargs -d '\n' gawk -f "${0%/*}/parse-desktop-entry.awk" \
 	| sort \
 	| gawk -F "\t" -v menu='bemenu -ipλ -fm all --fn "NimbusSans 10"' '
 		{
