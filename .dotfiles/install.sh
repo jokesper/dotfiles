e110fad949fc3fab0cfce0526561d28f1d65b554
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-13 10:14:31 +0200

    Added default network configuration

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -4,9 +4,21 @@ path=${0%/*}
 cd "$path/install/"
 install -Dm644 ./doas.conf -t /etc/
 install -Dm644 ./faillock.conf -t /etc/security/
+install -Dm644 ./25-wireless.network -t /etc/systemd/network/
 install -Dm644 ./personal.map -t /usr/local/share/kbd/keymaps/
 install -Dm644 ./personal-xkb -T /usr/share/X11/xkb/symbols/personal
 install -Dm644 ./vconsole.conf -t /etc/
 
+while IFS= read -r device; do
+	config="/etc/wpa_supplicant/wpa_supplicant-$device.conf"
+	[[ ! -f $config ]] && install -Dm600 ./wpa_supplicant.conf -T $config
+	systemctl enable "wpa_supplicant@$device"
+done <<< "$(networkctl list --json=short \
+	| jq -r '.Interfaces[]
+		| select(.Type == "wlan")
+		| .Name')"
+
 systemctl enable \
+	systemd-networkd \
+	systemd-resolved \
 	systemd-timesyncd \
