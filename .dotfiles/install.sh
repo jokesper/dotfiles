59ec528445d24787561c0b83498812c8cdbe637f
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-13 10:13:32 +0200

    Added enabling systemd-timesyncd in install script

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -7,3 +7,6 @@ install -Dm644 ./faillock.conf -t /etc/security/
 install -Dm644 ./personal.map -t /usr/local/share/kbd/keymaps/
 install -Dm644 ./personal-xkb -T /usr/share/X11/xkb/symbols/personal
 install -Dm644 ./vconsole.conf -t /etc/
+
+systemctl enable \
+	systemd-timesyncd \
