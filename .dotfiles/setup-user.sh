da97ba9fd2b386c401addb6841d8cec80cb76e10
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-15 00:33:57 +0200

    Added empty firefox config

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-cd "${0%/*}/defaults/"
+cd "${0%/*}/user/"
 config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
+ln -rs firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
 [[ ! -f "$config/update.sh" ]] && install -Dm755 update.sh -t "$config"
 [[ ! -f "$config/sway-config" ]] && install -Dm644 sway-config -t "$config"
