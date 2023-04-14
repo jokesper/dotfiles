7f282717133bd23d4173bfdb6d771ef7c1506a95
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-15 00:50:46 +0200

    Only add symlink for firefox config after firefox was started at least once

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -4,6 +4,6 @@ set -eu
 
 cd "${0%/*}/user/"
 config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
-ln -rs firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
+[[ -d ~/.mozilla ]] && ln -rs firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
 [[ ! -f "$config/update.sh" ]] && install -Dm755 update.sh -t "$config"
 [[ ! -f "$config/sway-config" ]] && install -Dm644 sway-config -t "$config"
