45be7e633f6fc2561ef86e864519512291389c71
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-15 00:20:55 +0200

    Changed user setup script to only add files if they don't exist

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -4,5 +4,5 @@ set -eu
 
 cd "${0%/*}/defaults/"
 config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
-install -Dm755 ./update.sh -t "$config"
-install -Dm644 ./sway-config -t "$config"
+[[ ! -f "$config/update.sh" ]] && install -Dm755 update.sh -t "$config"
+[[ ! -f "$config/sway-config" ]] && install -Dm644 sway-config -t "$config"
