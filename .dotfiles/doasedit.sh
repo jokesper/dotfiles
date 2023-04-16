773896427458ae136111e724e1634ec4850e2d28
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-16 21:51:45 +0200

    Added a way to edit files owned by root without giving root privileges to neovim

---
+++ .dotfiles/doasedit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+
+tmp="/tmp/$(tr -dc A-Za-z0-9 </dev/urandom | head -c12)-${2##*/}"
+mode=$(stat -c '%a' "$2")
+own=$(stat -c '%u:%g' "$2")
+
+cp "$2" "$tmp"
+chmod 600 "$tmp"
+chown "$DOAS_USER:" "$tmp"
+
+runuser -u "$DOAS_USER" -- $1 "$tmp"
+
+mv "$tmp" "$2"
+chmod $mode "$2"
+chown $own "$2"
