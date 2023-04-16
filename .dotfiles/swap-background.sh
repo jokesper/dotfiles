60ecab671e89f67daf750b2337a2b6c3d92c4df7
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-16 19:18:39 +0200

    Allowed more customization when specifing a wallpaper

--- .dotfiles/swap-background.sh
+++ .dotfiles/swap-background.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-set -eu
+#set -eu
 
-fallback='#0F0F0F'
-[[ ! -v 1 ]] &&
-	WPA_ID=$(wpa_cli status | awk -F= '/^id=/ {print $2; exit}')
-case ${2:-${WPA_ID:+CONNECTED}} in
-	CONNECTED)
-		ssid=$(wpa_cli get_network "$WPA_ID" ssid \
-			| tail -n 1 \
-			| jq --raw-output .)
-		swaymsg "output * background ~/Desktop/background-$ssid fit $fallback"
-		;;
-	*)
-		swaymsg "output * background $fallback solid_color"
-		;;
-esac
+set-wallpaper() {
+	wallpaper=$(find "$2" -type f 2>/dev/null | shuf -n1)
+	[[ -z "$wallpaper" ]] && return 1
+	swaymsg output "$1" background "$wallpaper" fit "#7F0000"
+}
+
+[[ ! -v 1 ]] && WPA_ID=$(wpa_cli status | awk -F= '/^id=/ {print $2; exit}')
+[[ (! -v 2 || $2 == 'CONNECTED') && -n "$WPA_ID" ]] &&
+	ssid=$(wpa_cli get_network "$WPA_ID" ssid \
+		| tail -n 1 \
+		| jq --raw-output .)
+
+swaymsg -t get_outputs \
+| jq -r '.[].name' \
+| while IFS= read -r output; do
+	set-wallpaper "$output" "$HOME/Desktop/background-${ssid:-offline}=$output" ||
+	set-wallpaper "$output" "$HOME/Desktop/background-${ssid:-offline}" ||
+	set-wallpaper "$output" "$HOME/Desktop/background=$output" ||
+	set-wallpaper "$output" "$HOME/Desktop/background" ||
+	swaymsg output "$output" background "#0F0F0F" solid_color
+done
