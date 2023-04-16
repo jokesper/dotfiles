4c9b3a083fef567e1ea0885f060b77fd82c5af43
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-04-16 15:28:16 +0200

    Autocreate Documents folder
    Launching neovim via Mod4+v would result in a working directory of / if Documents didn't exist, so instead of fixing it I autocreate the folder.

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+cd
+mkdir -p \
+	Documents \
+
 cd "${0%/*}/user/"
 config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
 [[ -d ~/.mozilla ]] && ln -rs firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
