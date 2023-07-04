6ff11aedc933e0e307e8183da5eb3c2d0ef62bd8
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-07-04 19:30:46 +0200

    Updated fish prompt to fit with my config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,8 @@
 if not status is-interactive; return; end
+
+function fish_prompt -d 'Write out the prompt'
+	printf '%s%s@%s%s:%s%s%s$ ' \
+		(set_color cyan) $USER $hostname (set_color normal) \
+		(set_color magenta) (basename (prompt_pwd)) (set_color normal) \
+
+end
