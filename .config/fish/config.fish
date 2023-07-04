cf363e25bc3247fcc3bdd7dda9ab0839e02f396b
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-07-04 20:16:30 +0200

    Changed fish keybindings to be vi style

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ function fish_prompt -d 'Write out the prompt'
 		(set_color magenta) (basename (prompt_pwd)) (set_color normal) \
 
 end
+function fish_mode_prompt -d 'Disable Indicator for modes'; end
 function fish_greeting -d 'Show unpushed git commits when starting fish'
 	git log -n8 @{u}..HEAD -- 2>/dev/null
 end
