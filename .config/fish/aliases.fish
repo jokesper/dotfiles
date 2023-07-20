d3a3e04924875334686281bdb6492677410ca1cc
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-07-20 23:02:05 +0200

    Added human readable flag to `ll` alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -14,7 +14,7 @@ end
 function down -w poweroff -d 'Quick alias to poweroff'; poweroff $argv; end
 function ls -w ls -d 'Color `ls` output'; command ls --color=auto -H $argv; end
 function grep -w grep -d 'Color `grep` output'; command grep --color=auto $argv; end
-function ll -w ls -d '`ls` but with long listing format'; ls -Al $argv; end
+function ll -w ls -d '`ls` but with long listing format'; ls -Alh $argv; end
 function la -w ls -d '`ls` but listing every item'; ls -A $argv; end
 function file -w file -d '`file` but with the `--brief` option'; command file -b $argv; end
 function man -w man -d '`man` but open in neovim if it is already running'; ~/.dotfiles/man.sh $argv; end
