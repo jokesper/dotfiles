dca9002ad133caf97dac7f7b96df4e0757c6bf70
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-11-05 16:36:17 +0100

    Removed rustup initialization in user setup

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -11,8 +11,6 @@ config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
 [[ -d ~/.mozilla ]] && ln -rsf firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
 [[ ! -f "$config/sway-config" ]] && install -Dm644 sway-config -t "$config"
 
-rustup default stable
-rustup component add rust-analyzer
 cargo install cargo-update rust-script
 
 hoogle generate
