e9a6fde64b720c708398a75ff87b387cffd5decc
jkeDev <52346246+jkeDev@users.noreply.github.com>
2023-11-06 22:08:41 +0100

    Started to migrate firefox config to dotfiles

--- .dotfiles/setup-user.sh
+++ .dotfiles/setup-user.sh
@@ -8,7 +8,6 @@ mkdir -p \
 
 cd "${0%/*}/user/"
 config=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/
-[[ -d ~/.mozilla ]] && ln -rsf firefox.js "$(find  ~/.mozilla/firefox/ -name '*.default-release')/user.js"
 [[ ! -f "$config/sway-config" ]] && install -Dm644 sway-config -t "$config"
 
 cargo install cargo-update rust-script
