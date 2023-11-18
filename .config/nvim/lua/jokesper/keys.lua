f688fe912a04c60251ff3e79b325228faabf52da
Joschua Kesper <jokesper@proton.me>
2023-11-18 13:17:41 +0100

    Removed removal of some keybindings in neovim as they are useful

--- .config/nvim/lua/jokesper/keys.lua
+++ .config/nvim/lua/jokesper/keys.lua
@@ -48,14 +48,6 @@ map('n', '<Leader>C', '"+c$')
 map('v', '<Leader>x', '"+x')
 map('v', '<Leader>X', '"+X')
 
--- Remove relative scrolling without moving cursor,
--- since it is only usefull after commands like `n`
--- and it should be in the autocmd
-local anti_bad_habits = '<Cmd>tabnew term://sl -Glwe<CR>'
-map('n', 'zt', anti_bad_habits)
-map('n', 'zz', anti_bad_habits)
-map('n', 'zb', anti_bad_habits)
-
 local v, max = vim.v, math.max
 map('v', 'J', function() return (":m '>+%i<CR>gv=gv"):format(max(1, v.count)) end, { expr = true })
 map('v', 'K', function() return (":m '<-%i<CR>gv=gv"):format(1 + max(1, v.count)) end, { expr = true })
