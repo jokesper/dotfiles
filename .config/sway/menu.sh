3abbd06d34377483dab8f7d0671a206117fcfd9b
Joschua Kesper <jokesper@proton.me>
2023-11-23 23:31:11 +0100

    Fixed minor bug in menu.sh

--- .config/sway/menu.sh
+++ .config/sway/menu.sh
@@ -5,8 +5,8 @@ set -eu
 find \
 	-L $(sed 's/:/\/applications /g' <(printf "%q" \
 		"${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}:")) \
-	-type f -name \*.desktop \
-	| xargs -d '\n' gawk -f "${0%/*}/parse-desktop-entry.awk" \
+	-type f -name \*.desktop -print0 \
+	| xargs -0 gawk -f "${0%/*}/parse-desktop-entry.awk" \
 	| sort \
 	| gawk -F "\t" -v menu='bemenu -ipλ -fm all --fn "NimbusSans 6"' '
 		{
